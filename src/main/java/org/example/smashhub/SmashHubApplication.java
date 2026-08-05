a93ae3e366e1956ce99a9e6366cd24da2205e29a
thanh <thanhlongbc28@gmail.com>
2026-08-05 16:56:30 +0700

    test commit

---
+++ src/main/java/org/example/smashhub/SmashHubApplication.java
@@ -0,0 +1,13 @@
+package org.example.smashhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmashHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmashHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/smashhub/SmashHubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smashhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmashHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
