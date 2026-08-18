5e0fe32c0a3092b3a6a450521c33368de840160c
thanh <thanhlongbc28@gmail.com>
2026-08-18 14:35:31 +0700

    exception

---
+++ src/main/java/org/example/smashhub/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package org.example.smashhub.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.smashhub.shared.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handlingRuntimeException(Exception exception) {
+        log.error("Exception: ", exception);
+        ApiResponse<Object> apiResponse = new ApiResponse<>();
+        apiResponse.setCode(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode());
+        apiResponse.setMessage(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(apiResponse);
+    }
+
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<ApiResponse<Object>> handlingAppException(AppException exception) {
+        ErrorCode errorCode = exception.getErrorCode();
+        ApiResponse<Object> apiResponse = new ApiResponse<>();
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleValidation(
+            MethodArgumentNotValidException exception) {
+
+        String errorKey = exception.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage();
+
+        ErrorCode errorCode = ErrorCode.valueOf(errorKey);
+
+        ApiResponse<Object> response = new ApiResponse<>();
+        response.setCode(errorCode.getCode());
+        response.setMessage(errorCode.getMessage());
+
+        return ResponseEntity
+                .status(errorCode.getStatusCode())
+                .body(response);
+    }
+}
